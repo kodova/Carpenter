c62b404607bcf511a3ea454faec89d30c6958056
Patrick Arnold <patrick@kodova.com>
2013-03-16 17:56:09 -0700

    Added documentation to fixture

--- carpenter-core/src/main/java/com/kodova/carpenter/fixture/Fixture.java
+++ carpenter-core/src/main/java/com/kodova/carpenter/fixture/Fixture.java
@@ -19,6 +19,10 @@ public abstract class Fixture<E> {
 
 	abstract public void configure(E entity);
 
+	/**
+	 * Creates a new instance of a entity of the type the fixture supports.
+	 * @return
+	 */
 	public E newInstance(){
 		Class<E> entityClass = getType();
 		try {
@@ -30,10 +34,19 @@ public abstract class Fixture<E> {
 		}
 	}
 
+	/**
+	 * Gets the class for the type of entity the fixture supports
+	 * @return
+	 */
 	public Class<E> getType(){
 		return (Class<E>) typeToken.getRawType();
 	}
 
+	/**
+	 * Sets the name of the fixture. This can bee used to override the default name as well as the @Name if one
+	 * exists on the fixture
+	 * @param name
+	 */
 	public void setName(String name){
 		this.name = Optional.of(name);
 	}
@@ -54,27 +67,58 @@ public abstract class Fixture<E> {
 		this.context = context;
 	}
 
+	public void setCarpenterCore(CarpenterCore carpenterCore) {
+		this.carpenterCore = carpenterCore;
+	}
+
+	/**
+	 * Gets an instance of another object from a fixture and will persist or build based on the original
+	 * request. If the original request is a build this will simply build. If the original request was a
+	 * create then this will create an instance of a persisted object from a fixture and persist it.
+	 * @param type
+	 * @param <T>
+	 * @return
+	 */
 	protected <T> T get(Class<T> type) {
 		return context.get(type);
 	}
 
+	/**
+	 * Will get and instance of another object from a fixture and override properties.
+	 * @see com.kodova.carpenter.fixture.Fixture#get(Class)
+	 * @param type
+	 * @param properties
+	 * @param <T>
+	 * @return
+	 */
 	protected <T> T get(Class<T> type, Properties properties) {
 		return context.get(type, properties);
 	}
 
+	/**
+	 * Configures a entity from a different fixture. Helpful for when you have two fixtures
+	 * of the same type with different names. You can create base fixture which you can call
+	 * from other fixtures using the composite method.
+	 * @param entity The entity that needs to be built
+	 * @param type The type fo the fixture you would like to configure onto the entity
+	 * @param <T>
+	 */
 	protected <T extends E> void composite(E entity, Class<T> type) {
 		composite(entity, type, type.getName());
 	}
 
+	/**
+	 * @see Fixture#composite(Object, Class)
+	 * @param entity The entity that needs to be built
+	 * @param type The type fo the fixture you would like to configure onto the entity
+	 * @param name The name of the fixture to call
+	 * @param <T>
+	 */
 	protected <T extends E> void composite(E entity, Class<T> type, String name){
 		Fixture<T> fixture = carpenterCore.getFixture(type, name);
 		if(fixture.equals(this)){
 			throw new RuntimeException("Can create composit from same fixture");
 		}
 		fixture.configure((T) entity);
-	}
-
-	public void setCarpenterCore(CarpenterCore carpenterCore) {
-		this.carpenterCore = carpenterCore;
 	}
 }
